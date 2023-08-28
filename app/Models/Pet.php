36115fbb69e2639a2493582d8a1d21839166a8c0
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-08-28 15:55:05 +0200

    Fatto seeder and migrate

--- app/Models/Pet.php
+++ app/Models/Pet.php
@@ -4,10 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Vaccination;
 
 class Pet extends Model
 {
     use HasFactory;
 
     protected $fillable = ['name', 'species', 'date_born', 'genre', 'owner', 'notes'];
+
+    public function vaccinations() {
+        return $this->hasMany(Vaccination::class);
+    }
 }
