a0873ca4de1be9374fa7fb008258204affea72e8
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-09-01 17:09:24 +0200

    lead mezzo fatto

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\Http\Requests\StoreLeadRequest;
+
+use App\Models\Lead;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LeadController extends Controller
+{
+    //
+}
