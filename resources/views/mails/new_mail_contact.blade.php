29baa3ef820d376653121f628c63593efb337dc9
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-09-04 12:48:38 +0200

    messaggi sistemati

--- resources/views/mails/new_mail_contact.blade.php
+++ resources/views/mails/new_mail_contact.blade.php
@@ -1,13 +1,13 @@
 <div class="container">
     <div class="row">
         <div class="col-12">
-            <h1 class="text-center">New contact received to form <span style="color:blue">PetDesk</span></h1>
+            <h1 class="text-center">Nuovo contatto ricevuto da <span style="color:blue">PetDesk</span></h1>
         </div>
     </div>
     <div class="row">
         <div class="col-12">
             <p>
-                <strong style="color:red">You have received a new messsage</strong> <br>
+                <strong style="color:red">Hai ricevuto un nuovo messaggio</strong> <br><br>
                 <strong>User</strong> {{ $lead->name }} <br>
                 <strong>Email</strong> {{ $lead->email }} <br>
                 <strong>Content</strong> {{ $lead->content }}
